a35aaa2c96b9771b8e5aa18e456554e29198b854
HPACBIO <hpacbio@gmail.com>
2025-11-27 03:46:50 +0100

    ProblemSection Skull

--- src/components/ui/oldPageComponents/hidden-skull.tsx
+++ src/components/ui/oldPageComponents/hidden-skull.tsx
@@ -9,7 +9,7 @@ export default function HiddenSkull() {
     const ref = useRef(null);
     const { scrollYProgress } = useScroll({
         target: ref,
-        offset: ["start end", "center center"],
+        offset: ["start end", "center start"],
     });
 
     return (
@@ -18,13 +18,13 @@ export default function HiddenSkull() {
             style={{
                 opacity: useTransform(scrollYProgress, [0, 1], [0, 1]),
             }}
-            className="relative w-full h-full grid place-items-center"
+            className="absolute -top-4/5 right-0 w-full h-full grid place-items-center mix-blend-color-burn"
         >
-            <div className="relative w-full h-full">
+            <div className="w-full h-[1200px]">
                 <img
                     src={typeof IconToxicSkull === 'string' ? IconToxicSkull : IconToxicSkull.src}
                     alt="IconToxicSkull"
-                    className="object-contain invert grayscale w-full h-full"
+                    className="object-contain grayscale w-full h-full"
                 />
             </div>
         </motion.div>
